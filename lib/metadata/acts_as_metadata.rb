710f6246a6817b9475f2dca5e00062c06ab925d1
gropher <grophen@gmail.com>
2011-10-12 23:56:32 +0400

    Bugs fixed.

--- lib/metadata/acts_as_metadata.rb
+++ lib/metadata/acts_as_metadata.rb
@@ -1,6 +1,5 @@
 module ActsAsMetadata
   def acts_as_metadata(options)
-		model = options[:model]
     if options[:scope].is_a?(Symbol) && options[:scope].to_s !~ /_id$/
       scope = "#{options[:scope]}_id".to_sym
     else
@@ -8,22 +7,20 @@ module ActsAsMetadata
 		end
 		
 		MetadataType.instance_variable_set("@metadata_scope", scope)
-		class_eval "@@metadata_scope = :#{scope}"
-    class_eval "@@metadata_model = :#{model}"
+		class_variable_set("@@metadata_scope", scope)
 		class_eval do
       has_many :metadata, :as => :model, :dependent => :destroy, :class_name => "Metadata::Metadata"
       
       def method_missing(meth, *args, &block)
-        type_names = MetadataType.model_types()
   	    if meth.to_s =~ /^(.+)=$/
   	      meth = meth[0..-2]
-  	      if type_names.include?(meth)
+  	      if metadata_types.include?(meth)
   	        set_metadata(meth, args.first)
   	      else
   	        super
   	      end
   	    else
-  	      if type_names.include?(meth)
+  	      if metadata_types.include?(meth)
   	        get_metadata(meth)
   	      else
   	        super
@@ -32,11 +29,11 @@ module ActsAsMetadata
   	  end
       
       def metadata_types
-        MetadataType.model_types(@@metadata_model.to_sym, self.send(@@metadata_scope))
+        MetadataType.model_types(self.class.name.to_sym, self.send(@@metadata_scope))
       end
 
 			def self.metadata_types(scope=nil)
-				types = MetadataType.model_types(@@metadata_model, scope)
+				types = MetadataType.model_types(self.class.name, scope)
         return types
 			end
       
@@ -49,11 +46,11 @@ module ActsAsMetadata
         value = value ? value : type.default
         self.metadata.where(:metadata_type => name).each{|m| m.destroy(true)}
         self.metadata.create({ :metadata_type => name, :value => value })
-				Rails.cache.delete("metadata_#{@@metadata_model}_#{self.id}")
+				Rails.cache.delete("metadata_#{self.class.name}_#{self.id}")
       end
 
 			def metadata_hash
-			  Rails.cache.fetch("metadata_#{@@metadata_model}_#{self.id}", :expires_in => 60.minutes) do
+			  Rails.cache.fetch("metadata_#{self.class.name}_#{self.id}", :expires_in => 60.minutes) do
 				  Hash[self.metadata.all.map { |m| [m.metadata_type, m.value] }]
 				end
 			end          
