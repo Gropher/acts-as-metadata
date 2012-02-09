bd46879e3198e9ec87fac08afa0ae765ffd1a2d6
gropher <grophen@gmail.com>
2012-02-09 18:14:09 +0400

    Minor bug fixed

--- lib/metadata/metadata_type.rb
+++ lib/metadata/metadata_type.rb
@@ -20,7 +20,7 @@ class MetadataType < ActiveRecord::Base
     :default, :format, :datatype, :values, 
     :models_json, :values_json, :default_json
   validates :tag, :presence => true, :format => {:with => /^[a-z]+[a-z0-9]*$/},
-    :exclusion => { :in => %w(format errors callback action categorie accept attributes host key layout notify open render save template type id parent_id lft rgt test),
+    :exclusion => { :in => %w(format errors callback action categorie accept attributes host key layout notify open render save template type id parent_id lft rgt test select),
      :message => "this name is reserved"}
   validates :datatype, :presence => true
   default_scope :conditions => {:deleted_at => nil}, :order => 'created_at DESC'
